3eff73cd5b783e09e74e013b97c8516bab6aa5c8
diinhill <hillenbrand.nadine@hotmail.de>
2021-08-19 00:37:02 +0200

    just fixing

--- src/components/CarouselItem.js
+++ src/components/CarouselItem.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Paper, Button } from '@material-ui/core'
+import BookPage from './BookPage'
 
 const CarouselItem = ({ item }) => {
 
@@ -14,6 +15,7 @@ const CarouselItem = ({ item }) => {
 
             <Button className='CheckButton'>
                 see full description
+                <BookPage bookKey={item.key} />
             </Button>
         </Paper>
     )
