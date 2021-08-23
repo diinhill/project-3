6bd9558a93c65f23e64f7ad88d5dcb1a4cdeb86b
diinhill <hillenbrand.nadine@hotmail.de>
2021-08-23 15:25:58 +0200

    firebase login & Register

--- src/components/BookPage.js
+++ src/components/BookPage.js
@@ -1,33 +1,54 @@
 import React, { useState, useEffect } from 'react'
 import BookCard from './BookCard'
 import { useParams } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
+import IconButton from '@material-ui/core/IconButton'
+import { Link } from 'react-router-dom'
 
 const BookPage = () => {
 
-    const { bookKey } = useParams()
-    console.log('bookKey:', bookKey)
+    const history = useHistory()
+
+    const { selectedEdition } = useParams()
+    console.log('selectedEdition:', selectedEdition)
 
     const [bookInfo, setBookInfo] = useState()
+    const [workInfo, setWorkInfo] = useState()
+    const [selectedAuthorKey, setSelectedAuthorKey] = useState('')
 
 
     useEffect(() => {
         const getBookInfo = async () => {
-            const response = await fetch(`https://cab-cors-anywhere.herokuapp.com/https://openlibrary.org/works/${bookKey}.json`)
-            const obj = await response.json()
-            console.log('obj:', obj)
-            setBookInfo(obj)
+            const response = await fetch(`https://cab-cors-anywhere.herokuapp.com/http://openlibrary.org/search.json?q=${selectedEdition}`)
+            const books = await response.json()
+            console.log('books.docs:', books.docs)
+            console.log('books.docs.author_key:', books.docs[0].author_key[0])
+            setBookInfo(books.docs[0])
+            setSelectedAuthorKey(books.docs[0].author_key[0])
+
+            const getWorkInfo = async () => {
+                const response = await fetch(`https://cab-cors-anywhere.herokuapp.com/https://openlibrary.org/${books.docs[0].key}.json`)
+                const work = await response.json()
+                console.log('work:', work)
+                setWorkInfo(work)
+            }
+            getWorkInfo()
         }
         getBookInfo()
-    }, [bookKey])
-    console.log('bookInfo:', bookInfo)
 
+    }, [selectedEdition])
+    // console.log('bookInfo:', bookInfo)
+    // console.log('workInfo:', workInfo)
 
 
     return (
 
         <div style={{ margin: 'auto' }}>
-            {bookInfo &&
-                <BookCard bookInfo={bookInfo} />
+            {bookInfo && workInfo &&
+                <BookCard bookInfo={bookInfo} workInfo={workInfo} />}
+            {    <IconButton aria-label="show author details">
+                    <Link to={`/authors/${selectedAuthorKey}`} />
+                </IconButton>
             }
         </div>
     )
